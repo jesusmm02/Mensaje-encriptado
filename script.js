d1aee01243fdde7aadea6143da3dd9ae75407d12
jmelara02 <jesusmelara77@gmail.com>
2024-04-04 11:16:08 +0200

    Agregación de un elemento cambiante
    Se ha añadido un elemento que al hacer clic en él cambia su apariencia y muestra un mensaje por pantalla.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let sign = prompt("MENSAJE SECRETO:");
 let mensaje=sign.toUpperCase();
+let image = document.getElementById("candado");
 
 function reemplazarLetras(mensaje) {
 const letras = mensaje.split('');
@@ -50,3 +51,17 @@ console.log(cadenaReemplazada);
 
 const resultadoElement = document.querySelector('.resultado');
 resultadoElement.textContent = cadenaReemplazada;
+
+let nameImage;
+image.addEventListener("click", () => {
+  nameImage = image.src.substring(image.src.lastIndexOf('/') + 1, image.src.length)
+  if (nameImage == './Imagenes/abierto.png') {
+    image.src = "./Imagenes/cerrado.png";
+  } else {
+    image.src = "./Imagenes/abierto.png";
+    let datos = document.getElementById("mensajeOculto");
+    datos.innerHTML= `
+    <h1 style="background-color: black">MENSAJE RECIBIDO CON ÉXITO</h1>
+    `;
+  }
+})
